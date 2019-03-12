d1439c6ece9add0a9e355d0b7fc5ea2c16c18be9
Ichigo Daifuku <owen.lee@icloud.com>
2019-03-12 11:33:41 +1100

    Update guiPlaceholder.py
    Generalised the gui

--- lib/gui/guiPlaceholder.py
+++ lib/gui/guiPlaceholder.py
@@ -408,115 +408,9 @@ class ComplexImage(object):
         for i in self.images: 
             i.hide()
 
-#--------------------------------------------------------------------
-#--------------------------------------------------------------------
-#--------------------------------------------------------------------
-
-class GuiOverworldEntities(ComplexImage):
-    """Overworld entity class"""
-    animationSpeed = 2
-    imagePrefix = 'entity'
-    animations = {
-        False: ['b', 'n'], #Idle
-        True: ['l', 'r'], #Walking
-    }
-    def __init__(self, layer=1):
-        self.x = 0
-        self.y = 0
-        self.layer = layer
-        self.images = []
-
-    def cleanup(self, l):
-        excessImages = len(self.images) - len(l)
-        for _ in range(excessImages):
-            self.images.pop(0)
-
-        for _ in range(-excessImages):
-            self.images.append(DormantImage(0, 0, 'blank'))
-
-    def update(self, l):
-        self.hide()
-        self.cleanup(l)
-
-        for i, entity in enumerate(l):
-            x, y = entity.position.toPixels()
-            image = self.entityName(entity)
-            self.images[i].update(image, sx=x, sy=y)
-        self.show()
-
-    def layersort(self, x, y):
-        """Sorts images by ycoord"""
-        a = cmp(y.y, x.y)
-        if a == 0: 
-            return cmp(y.x, x.x)
-        return a
-
-    @staticmethod
-    def entityName(e):
-        """Returns the entitys image name"""
-        name = e.name
-        facing = str(e.facingLinear)
-        prefix = GuiOverworldEntities.imagePrefix
-        legdir = GuiOverworldEntities.entityLegdir(e)
-        return '-'.join([prefix, name, facing, legdir])
-
-    @staticmethod
-    def entityLegdir(e):
-        """docstring for entityLegdir"""
-        if e.animationState['frame'] >= GuiOverworldEntities.animationSpeed:
-            e.animationState['frame'] = 0
-            e.animationState['state'] = not(e.animationState['state']) #To 1 or 0. Toggles
-        legdir = GuiOverworldEntities.animations
-        return legdir[e.hasMoved][e.animationState['state']]
-
-class GuiOverworldTerrain(ComplexImage):
-    """docstring for Terrain"""
-    def __init__(self, layer=1):
-        self.x = 0
-        self.y = 0
-        self.layer = layer
-        self.images = []
-        self.terrain = []
-        for x in range(BoardWidth):
-            a = []
-            for y in range(BoardHeight):
-                img = DormantImage(x*32, y*32, 'terrain-debug1', layer=4)
-                a.append(img)
-            self.images.extend(a)
-            self.terrain.append(a)
-        super(GuiOverworldTerrain, self).__init__()
-
-    def update(self, board):
-        for x in range(BoardWidth):
-            for y in range(BoardHeight):
-                image = board[x, y].image()
-                image = ImageName(image)
-                self.terrain[x][y].update(image)
-
-class Board(ComplexImage):
-    """Board for gui"""
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.layer = 4
-        self.terrain = GuiOverworldTerrain(layer=4)
-        self.entities = GuiOverworldEntities(layer=2)
-        self.images = [self.terrain, self.entities]
-        super(Board, self).__init__()
-        
-    def update(self, board, entities):
-        self.terrain.update(board)
-        self.entities.update(entities)
-
-    def show(self):
-        self.terrain.show()
-        
-#--------------------------------------------------------------------
-#--------------------------------------------------------------------
-#--------------------------------------------------------------------
-
-class Gui(ComplexImage):
-    """The Gui main"""
+            
+class GuiBase(ComplexImage):
+    """tkinter base gui module"""
     def __call__(self, objpath, call, *args, **kwargs):
         #try:
         if objpath == 'update':
@@ -543,20 +437,14 @@ class Gui(ComplexImage):
 
     def __init__(self):
         """Class constructor"""
-        global tk
-        global imageDict, _canvas, sprites
-        global app_width, app_height
-        res = 1
+        global tk, imageDict, _canvas, sprites, app_width, app_height
         tk = Tk()
-        _canvas = Canvas(tk, width=base_size[0]+tk_border, height=base_size[1]+tk_border, takefocus=True)
-        _canvas.pack()
-        self.canvas = _canvas
+        self.canvas = Canvas(tk, width=base_size[0]+tk_border, height=base_size[1]+tk_border, takefocus=True)
+        self.canvas.pack()
+        _canvas = self.canvas
         sprites = SpriteDict()
         imageDict = _ImageDictionary()
-
-        self.images = {
-            'board': Board(),
-        }
+        self.images = {}
 
     def destroy(self):
         tk.destroy()

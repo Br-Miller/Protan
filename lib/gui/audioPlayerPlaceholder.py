e9ff525c673ee511244b766bfe8206e94558ba21
Ichigo Daifuku <owen.lee@icloud.com>
2019-03-12 11:57:07 +1100

    Update audioPlayerPlaceholder.py

--- lib/gui/audioPlayerPlaceholder.py
+++ lib/gui/audioPlayerPlaceholder.py
@@ -1,6 +1,27 @@
 """Audio playing class
-Contains functions relating to the playing, contencating, looping and editing
-of audio files
+Plays, contencates, loops and edits audio files
+Currently only works on OS
 """
 
 
+import os
+import commands
+from subprocess import call
+
+
+def terminalDoOs(s):
+    os.system(s)
+
+def terminalDoSub(l):
+    call(['ls', '-l'])
+
+def terminalDoCom(s):
+    s=commands.getstatusoutput(s)
+    return s
+
+
+def play(filepath):
+    terminalDoSub(['afplay', filepath, '&'])
+
+#afplay filepath &
+#killall afplay

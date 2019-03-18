30717f435fb4e1559983d8b9024179b1aedd4a0c
Ichigo Daifuku <owen.lee@icloud.com>
2019-03-18 16:17:41 +1100

    Completed addObjectData

--- lib/developer/tools/addObjectData.py
+++ lib/developer/tools/addObjectData.py
@@ -1,30 +1,74 @@
-"""File to add data to dat files
-
-"""
+"""Module to add data to data files"""
 
 
 import os
 import sys
 import json
+from warnings import warn
 
-data = {
+path = {
     'entity': './dat/entity.dat',
     'terrain': './dat/terrain.dat',
 }
+data = {}
 
 if __name__ = '__main__':
     path = os.path.abspath('./../../')
     os.chdir(path)
     sys.path.append(path)
   
+
 def addEntity(d):
     pass
-  
+
+def dumpjson(filepath, x):
+    with open(filepath, 'w') as f:
+        x = json.dumps(x)
+        f.write(x)
+
 def loadjson(filepath):
-    pass
-  
+    try:
+        f = open(filepath)
+        c = f.reads()
+        c = json.loads(c)
+        f.close()
+        return c
+    except IOError:
+        dumpjson(filepath, {})
+        return {}
+
+def saveData():
+    for k, v in path.items()
+        dumpjson(v, data[k])
+    
 def loadData():
     d = {}
-    for name, path in paths.keys():
+    for k, v in path.items():
+        d.update({k: loadjson(v)})
+    data.update(d)
+    
+def nullfunction():
+    pass
+    
+def main():
+    d = {
+        'addentity': addEntity,
+    }
+    r = True
+    f = nullfunction
+    loadData()
+    
+    while r:
+        cmd = raw_input('> ')
+        fnc = d.get(cmd, f)
+        fnc()
+        if cmd == 'quit':
+            r = False
+            
+        if cmd == 'save':
         
-    path.update(d)
+
+if __name__ == '__main__':
+    main()
+else:
+    warn(Warning('This module is not meant to be imported'))

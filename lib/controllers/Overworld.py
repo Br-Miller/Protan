5e148ff802a05b06a0fb8372a790b49f4e6fe5b6
Ichigo Daifuku <owen.lee@icloud.com>
2019-03-22 11:37:12 +1100

    Create Overworld.py

---
+++ lib/controllers/Overworld.py
@@ -0,0 +1,49 @@
+"""Game overworld stuff"""
+
+import os, sys
+
+if __name__ == '__main__':
+  sys.path.append( os.path.abspath('./../lib') )
+  os.chdir('./../lib')
+
+from gameobj.board import MapArea
+from gameobj.entities import EntityOverworld
+from mixins.CoordPos import AxisDistance, BasicRectangleArea, Coordinate, Direction
+from gui.guiMain import Gui
+
+gui = Gui()
+gui('board.show', True)
+entityA = EntityOverworld(Coordinate((0, 0, 0), (0, 0, 0)), {'id':'amberSoldier' , 'name': 'amberSoldier', 'facing': 'n', 'move': {'t': 0, 'px': 2, 'sx': 0}, 'customHitbox': None})
+entityB = EntityOverworld(Coordinate((7, 0, 0), (7, 0, 0)), {'id': 'debug1', 'name': 'debug1', 'facing': 's', 'move': {'t': 0, 'px': 2, 'sx': 0}, 'customHitbox': None})
+board = MapArea(AxisDistance(t=30), AxisDistance(t=20), b='.debug1')
+entities = [entityA, entityB]
+
+def getInput():
+  return raw_input('input direction: ')
+
+def update():
+  gui('board.update', True, board, entities)
+  gui('update', True)
+
+def mainA():
+  while True:
+    update()
+    a = getInput()
+    a = a.strip()
+    if Direction.isDir(a):
+      entityA.move(board, entities, face=a)
+
+def main():
+  for i in range(60):
+    entityA.tick()
+    entityB.tick()
+    update()
+  for i in range(224):
+    entityA.tick()
+    entityB.tick()
+    entityA.move(board, entities, face='ne')
+    update()
+
+
+if __name__ == '__main__':
+  main()

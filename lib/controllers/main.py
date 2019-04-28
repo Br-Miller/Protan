43d958c6e419d7f2cc431ac30ec30cf79ccf77f3
Ichigo Daifuku <owen.lee@icloud.com>
2019-04-28 15:19:26 +1000

    Update main.py

--- lib/controllers/main.py
+++ lib/controllers/main.py
@@ -10,45 +10,57 @@ if __name__ == '__main__':
   sys.path.append(os.path.abspath('../'))
   os.chdir('../')
 
+from mixins.fileLS import FileLS
 from gameobj.board import MapArea
 from gameobj.entities import OverworldEntity
-from gui import guiPlaceholder
+from gui import guiMain
 
 
 keybindFile = './dat/tkinterKeybinds.json'
-
+keybinds = FileLS.load(keybindFile, s='json')
 
 class OverworldMode(object):
   """Overworld"""
   def __init__(self, arg):
-    self.player = entities()
+    self.player = OverworldEntity()
     self.board = MapArea()
     
-class main(object):
-  """Main"""
+  def tick(self):
+    #check keyboard
+    #Move player
+    #Move other entities
+    #
+    
+class Main(object):
   def __init__(self):
-    global gui
-    gui = guiPlaceholder.Gui()
+    self.gui = guiMain.Gui()
+    self.userinput = guiMain.UserInput(list(keybinds.values()))
     
   def start(self):
     """Starts the game"""
-    f = open(keystrokesFile)
-    keybinds = json.load(f)
-    f.close()
-
-    allKeys = {}
-    l = ['lowercase', 'uppercase', 'functions', 'numbers', 'symbols']
+    self.bindKeys()
+    self.bindButtons()
+    guiMain.tk.after(16, func=self.tick)
+    guiMain.mainloop()
     
-    for i in l: 
-      allKeys.update(keybinds[i])
+  def bind(self, s, f):
+    gui._canvas.bind_all(s, f)
 
-    for key in allKeys:
-      gui._canvas.bind_all(key, self.handle_ki) 
-    gui._canvas.bind_all('<Button-1>', self.handle_rmsi)
-    gui._canvas.bind_all('<Button-2>', self.handle_lmsi)
-    gui._canvas.bind_all('<Motion>', self.handle_mi)
-    guiPlaceholder.mainloop()
-
+  def bindKeys(self):
+    l = ['KeyPress', 'KeyRelease']
+    for k, v in keybinds.items():
+      self.bind('<KeyPress-{}>'.format(k), self.userinput.keyPress)
+      self.bind('<KeyRelease-{}>'.format(k), self.userinput.keyRelease)
+    
+  def bindButtons(self):
+    self.bind('<Motion>', self.userinput.mouseMove)
+    self.bind('<ButtonPress-1>', self.userinput.mousePress)
+    self.bind('<ButtonRelease-1>', self.userinput.mouseRelease)
+    #gui._canvas.bind_all('<Button2>', self.userinput)
+    
+  def tick(self):
+    pass
+    
   def destroy(self):
     try: 
       gui.destroy()
@@ -58,32 +70,4 @@ class main(object):
 
   def handle(self, ginput):
     self.game.handle(ginput)
-
-  def handle_ki(self, pinput):
-    """Handles player's key input"""
-    input_data = ('Key', pinput.keysym)
-    self.handle(input_data)
-
-  def handle_mi(self, minput):
-    """Handles player's mouse movement"""
-    self.handle(('MouseMove', (minput.x - gui_module.tk_border, minput.y - gui_module.tk_border)))
-
-  def handle_rmsi(self, minput):
-    """Handles player's mouse's right single click's"""
-    self.handle(('SRightPress', (minput.x - gui_module.tk_border, minput.y - gui_module.tk_border)))
-
-  def handle_rmdi(self, minput):
-    """Handles player's mouse's right double click's"""
-    self.handle(('DRightPress', (minput.x - gui_module.tk_border, minput.y - gui_module.tk_border)))
-
-  def handle_lmsi(self, minput):
-    """Handles player's mouse's left single click's"""
-    self.handle(('SLeftPress', (minput.x - gui_module.tk_border, minput.y - gui_module.tk_border)))
-
-  def handle_lmdi(self, minput):
-    """Handles player's mouse's left double click's"""
-    self.handle(('DLeftPress', (minput.x - gui_module.tk_border, minput.y - gui_module.tk_border)))
-
-  def handle_mwi(self, minput):
-    """Handles player's mousewheel"""
-    self.handle(('MouseWheel', (minput.x, minput.y)))
+    
